540739324a463622951099d18e59adad9b9f1410
Mofeng <363034937@qq.com>
2021-09-01 23:56:06 +0800

    修复加载阻塞导致无法打卡bug

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ import smtplib
 from email.header import Header
 from email.mime.text import MIMEText
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+# from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
-import os
+# import os
 
 
 # ####DEF PART#####
-# def getLoginInfo():
+
+
 def isElementExists_byXpath(browser, elementXpath):
     isExists = True
     try:
@@ -42,8 +46,8 @@ def emailModule(subject, text):
     # 邮件内容部分
 
     msg = MIMEText(text)  # 主体，同时赋予文本
-    msg['From'] = Header("HFUT-自动打卡", 'utf-8')  # 发送者
-    # msg['To'] = Header("", 'utf-8')  # 接收者
+    msg['From'] = Header("HFUT-自动打卡", 'utf-8')  # 发送者昵称
+    # msg['To'] = Header("", 'utf-8')  # 接收者昵称
     msg['Subject'] = Header(subject, 'utf-8')  # 标题
 
     # 邮件发送部分
@@ -57,42 +61,60 @@ def emailModule(subject, text):
 
 # #####MAIN######
 
+# 隐藏窗口部分
+
+br: webdriver
+chrome_options = Options()
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--disable-gpu')
+
+# 服务器自用部分:隐藏窗口
+'''
+chrome_options = Options()
+chrome_options.add_argument('--headless')  #
+chrome_options.add_argument('disable-dev-shm-usage')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('no-sandbox')
+'''
 
 try:
     # 从config.json读取驱动路径
     configFile = open("config.json", "r", encoding="utf-8")
     configInfo = json.loads(configFile.read())
+    configFile.close()
     PATH = configInfo['驱动路径']
     # 打开浏览器
-    br = webdriver.Chrome(executable_path=PATH)
-    # 打开HFUT学工工作平台,并登录HFUT CAS统一认证平台
-    br.get("https://cas.hfut.edu.cn/cas/login")
+    br = webdriver.Chrome(executable_path=PATH, options=chrome_options)
+
+    # 进入学生疫情信息收集页面
+
+    br.get("http://stu.hfut.edu.cn/xsfw/sys/xsyqxxsjapp/*default/index.do#/mrbpa")
+    sleep(random.randint(5, 10))
+    br.find_element_by_xpath("/html/body/div[1]/div/div/div/div/button[1]").click()
+    sleep(random.randint(5, 10))
 
     # 从config.json读取用户名(学号)，密码并登录
-    # print(configInfo.items())#for debug
+
     username = configInfo['学号']
     password = configInfo['密码']
-    configFile.close()
+
     # 填入学号密码
+
     br.find_element_by_id("username").send_keys(username)
     br.find_element_by_id("pwd").send_keys(password)
 
     # 点击登录
-    br.find_element_by_id("sb2").click()
 
-    if isElementExists_byXpath(br, '/html/body/div[2]/div/div[2]/form/div[2]/div/span/div/span'):
+    br.find_element_by_id("sb2").click()
+    sleep(random.randint(5, 10))
+    if isElementExists_byXpath(br, '/html/body/div[2]/div/div[2]/form/div[2]/div/span/div/span'):  # 登陆失败
         br.close()
         print("学号/密码错误！")
         raise Exception
-    # 进入学生疫情信息收集页面
-
-    br.get("http://stu.hfut.edu.cn/xsfw/sys/xsyqxxsjapp/*default/index.do#/mrbpa")
-    sleep(random.randint(3, 5))
-    br.find_element_by_xpath("/html/body/div[1]/div/div/div/div/button[1]").click()
-    sleep(random.randint(3, 5))
 
     # 判断今日是否未填报
-    if isElementExists_byXpath(br, '//*[@id="save"]') == False:
+
+    if not isElementExists_byXpath(br, '//*[@id="save"]'):
 
         if isElementExists_byXpath(br, '/html/body/main/article/section/div/div[3]/div[2]/div/div[4]/div['
                                        '2]/div/table/tbody/tr[1]/td[2]/a[1]'):
@@ -106,11 +128,14 @@ try:
         # 点击保存按钮，提交打卡信息
         br.find_element_by_xpath('//*[@id="save"]').click()
         print("自动打卡成功！")
-        emailModule("自动打卡成功！", "于" + time.strftime("%Y-%m-%d-%H时%M分%S秒", time.localtime()) + "进行了自动打卡")
+        emailModule("自动打卡成功！", username + "于" + time.strftime("%Y-%m-%d-%H时%M分%S秒", time.localtime()) + "进行了自动打卡")
 
     br.close()
 except:
-    br.close()
-    print("自动打卡失败！"", 程序运行出现错误")
-    os.system('pause')
-    emailModule("自动打卡失败！", "程序运行出现错误")
+    try:
+        br.close()
+        print("自动打卡失败！"" 程序运行出现错误")
+        emailModule("自动打卡失败！", "程序运行出现错误")
+    except:
+        print("自动打卡失败！""Selenium配置出现问题，打开模拟浏览器失败")
+        emailModule("自动打卡失败！", "程序运行出现错误")
